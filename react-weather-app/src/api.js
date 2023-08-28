0433788a2a91a552a0135d96561a3f412197be51
otmp <47669979+Ryltc@users.noreply.github.com>
2023-08-28 18:03:50 -0400

    creation of weather app

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions  = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1957b42c64msh7ade56fd61d0085p1668a9jsn5f229fd30bb9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.radpidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
